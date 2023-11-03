66ec566d69a9239414291a3424a6a5875dcc085b
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-11-03 10:05:49 -0400

    Initial Commit

---
+++ appsync_models_api/appsync_models_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsync_models_apiApp.swift
+//  appsync_models_api
+//
+//  Created by Saultz, Ian on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct appsync_models_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
